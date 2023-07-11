670f3793895b54b228a2cd43749923f0d2514c1c
Saumyaranjan Parida <saumyaranjan.parida@Saumyaranjans-MacBook-Pro.local>
2023-07-11 12:25:22 +0530

    Added Ui

--- server/src/polls/polls.module.ts
+++ server/src/polls/polls.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { jwtModule, redisModule } from 'src/modules.config';
 import { PollsController } from './polls.contoller';
+import { PollsGateway } from './polls.gateway';
+import { PollsRepository } from './polls.repository';
 import { PollsService } from './polls.service';
 
 @Module({
-  imports: [ConfigModule],
+  imports: [ConfigModule, redisModule, jwtModule],
   controllers: [PollsController],
-  providers: [PollsService],
+  providers: [PollsService, PollsRepository, PollsGateway],
 })
 export class PollsModule {}
